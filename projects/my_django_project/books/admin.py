6f072535d5178eeda1f6fb06d855386ad2ba513e
danapython <danapython@example.com>
2016-09-11 19:40:36 +0300

    Add new Author model, create relationship between Book and Author

--- projects/my_django_project/books/admin.py
+++ projects/my_django_project/books/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Book
+from .models import Author, Book
 
 # Register your models hoere.
 
 admin.site.register(Book)
+admin.site.register(Author)
